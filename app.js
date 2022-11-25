cd92c228b48698e456db06969147cd325628963e
RevanRehimli313 <revanrehimli02@gmail.com>
2022-11-25 14:19:48 +0400

    add new task

---
+++ app.js
@@ -0,0 +1,24 @@
+// function add (ed1 , ed2 , callback){
+//     let netice = ed1 + ed2
+//     callback(netice)
+//     }
+// add(3 , 5 , function(netice){
+//     console.log('Netice:' , netice)
+// }
+// )
+// if(prompt = 'console'){
+//     console.log(netice)
+// }
+function add( ed1 , ed2, callback){
+    prompt('Netice harada gosterilsin? ')
+    let netice = ed1 + ed2
+
+}
+add(3 , 5)
+add(callback)
+ if(prompt = 'console'){
+        console.log(netice)
+    }
+    if(prompt = 'alert'){
+        alert(netice)
+    }
